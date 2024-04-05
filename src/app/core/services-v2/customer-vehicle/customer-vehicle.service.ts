587add203fb71f696283446cde577b460aed79aa
Fabio <fabio_alderete_s@hotmail.com>
2024-04-05 15:51:38 -0300

    add new services!

---
+++ src/app/core/services-v2/customer-vehicle/customer-vehicle.service.ts
@@ -0,0 +1,47 @@
+// Angular
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// Environment
+import { environment } from '@env/environment';
+
+const API_CUSTOMER = `${environment.apiEcommerce}/api/v1/customer`;
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ArticleService {
+    constructor(private http: HttpClient) {}
+
+    getAll(documentId:string){
+        return this.http.get(`${API_CUSTOMER}/${documentId}/customer-vehicle`)
+    }
+
+    getPaginatedCustomerVehicles(
+        documentId:string,
+        params:{
+            page?:number,
+            limit?:number,
+            sort?:string
+        }){
+        return this.http.get(`${API_CUSTOMER}/${documentId}/paginated-customer-vehicle`,{params:params})
+    }
+
+    createCustomerVehicle(
+        documentId:string,
+        params:{
+            patent:string,
+            brand:string,
+            model:string,
+            typeVehicle:string,
+            manufactureYear:number,
+            codeMotor:string,
+            codeChasis:string,
+            customer:string,
+            typeImp:string,
+            detail:string,
+            codeSii:string
+        }){
+        return this.http.post(`${API_CUSTOMER}/${documentId}/customer-vehicle`,params)
+    }
+
+}
