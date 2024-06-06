2fae19d197c75abf4de2a3d45caf3de4f6a0279b
Fabio <fabio_alderete_s@hotmail.com>
2024-06-06 17:50:54 -0400

    buyer only line credit and order attributes articles

--- src/app/modules/shop/components/detalle-tecnicos/detalle-tecnicos.component.ts
+++ src/app/modules/shop/components/detalle-tecnicos/detalle-tecnicos.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../../../shared/interfaces/product';
-import { IArticleResponse } from '@core/models-v2/article/article-response.interface';
+import { IArticleResponse, IAttribute } from '@core/models-v2/article/article-response.interface';
 
 @Component({
   selector: 'app-detalle-tecnicos',
@@ -14,10 +14,17 @@ export class DetalleTecnicosComponent implements OnInit {
   innerWidth!: number;
   videoWidth = 0;
   videoHeight = 0;
+  attributes:IAttribute[] | undefined = []
+  /**
+   * Order Marca-largo-ancho-alto-peso
+   */
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.attributes = this.producto?.attributes
+    this.orderAttributes()
+  }
 
   getIdVideo(url: string) {
     const idVideo = url.split('/');
@@ -36,6 +43,41 @@ export class DetalleTecnicosComponent implements OnInit {
     } else {
       this.videoWidth = 300;
       this.videoHeight = 150;
+    }
+  }
+
+  orderAttributes(){
+    if(this.attributes !== undefined){
+      let iPeso:number = this.attributes?.findIndex(x => x.name === 'PESO')
+      if(iPeso != -1){
+        let attPeso = this.attributes[iPeso]
+        this.attributes.splice(iPeso,1)
+        this.attributes.unshift(attPeso)
+      }
+      let iAlto:number = this.attributes?.findIndex(x => x.name === 'ALTO')
+      if(iAlto != -1){
+        let attAlto = this.attributes[iAlto]
+        this.attributes.splice(iAlto,1)
+        this.attributes.unshift(attAlto)
+      }
+      let iAncho:number = this.attributes?.findIndex(x => x.name === 'ANCHO')
+      if(iAncho != -1){
+        let attAncho = this.attributes[iAncho]
+        this.attributes.splice(iAncho,1)
+        this.attributes.unshift(attAncho)
+      }
+      let iLargo:number = this.attributes?.findIndex(x => x.name === 'LARGO')
+      if(iLargo != -1){
+        let attLargo = this.attributes[iLargo]
+        this.attributes.splice(iLargo,1)
+        this.attributes.unshift(attLargo)
+      }
+      let iMarca:number = this.attributes?.findIndex(x => x.name === 'MARCA')
+      if(iMarca != -1){
+        let attMarca = this.attributes[iMarca]
+        this.attributes.splice(iMarca,1)
+        this.attributes.unshift(attMarca)
+      }
     }
   }
 }
