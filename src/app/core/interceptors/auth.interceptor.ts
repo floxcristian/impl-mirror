c7298be7fbe1ff9d1cdb22f22b3e2d779108c398
Ricardo Rojas <ricardo.rojasortega@gmail.com>
2024-06-29 19:18:56 -0400

    rollback interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -6,7 +6,6 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse,
 } from '@angular/common/http';
 // Rxjs
 import { Observable, catchError, switchMap, throwError } from 'rxjs';
@@ -39,20 +38,13 @@ export class AuthInterceptor implements HttpInterceptor {
 
     const session = this.sessionStorageService.get();
     const tokens = this.sessionTokenStorage.get();
-    const isMultipartFormData = request.body instanceof FormData;
-
     if (tokens && request.url.includes('api/v')) {
       const accessToken = tokens.accessToken;
-      let headers = request.headers
-        .set('Authorization', 'Bearer ' + accessToken)
-        .set('X-Ecommerce-User', session?.email || '');
-
-      // No sobrescribimos el Content-Type si es multipart/form-data
-      if (!isMultipartFormData) {
-        headers = headers.set('Content-Type', 'application/json');
-      }
-
-      newReq = request.clone({ headers });
+      newReq = request.clone({
+        headers: request.headers
+          .set('Authorization', 'Bearer ' + accessToken)
+          .set('X-Ecommerce-User', session?.email || ''),
+      });
     } else {
       if (isPlatformBrowser(this.plataformaId)) {
         this.authdata = window.btoa(
@@ -62,32 +54,30 @@ export class AuthInterceptor implements HttpInterceptor {
         this.authdata = 'c2VydmljZXM6MC49ajNEMnNzMS53Mjkt';
       }
 
-      let headers = request.headers
-        .set('Authorization', 'Basic ' + this.authdata)
-        .set('X-Ecommerce-User', session?.email || '');
-
-      // No sobrescribimos el Content-Type si es multipart/form-data
-      if (!isMultipartFormData) {
-        headers = headers.set('Content-Type', 'application/json');
-      }
-
-      newReq = request.clone({ headers });
+      newReq = request.clone({
+        headers: request.headers.set(
+          'Authorization',
+          'Basic ' + this.authdata
+        ),
+      });
     }
 
     if (request.url.includes('api/v1/auth/refresh')) {
       return next.handle(newReq).pipe(
-        catchError((error: HttpErrorResponse) => {
+        catchError((error) => {
           if (error.status === 401 && tokens) {
             this.logoutService.clearSession();
             this.logoutService.irAInicio();
+          } else {
+            return throwError(() => new Error(error));
           }
-          return throwError(() => error);
+          return throwError(() => new Error(error));
         })
       );
     }
 
     return next.handle(newReq).pipe(
-      catchError((error: HttpErrorResponse) => {
+      catchError((error) => {
         if (error.status === 401 && tokens) {
           const refreshToken = tokens.refreshToken;
           if (refreshToken) {
@@ -95,31 +85,24 @@ export class AuthInterceptor implements HttpInterceptor {
               switchMap((resp) => {
                 this.sessionTokenStorage.set(resp);
                 const accessToken = resp.accessToken;
-                let headers = newReq.headers.set(
-                  'Authorization',
-                  'Bearer ' + accessToken
-                );
-
-                // No sobrescribimos el Content-Type si es multipart/form-data
-                if (!isMultipartFormData) {
-                  headers = headers.set('Content-Type', 'application/json');
-                }
-
-                newReq = newReq.clone({ headers });
+                newReq = request.clone({
+                  headers: request.headers.set(
+                    'Authorization',
+                    'Bearer ' + accessToken
+                  ),
+                });
                 return next.handle(newReq);
               }),
-              catchError((refreshError) => {
-                console.error(
-                  'Error al obtener refresh tokens: ',
-                  refreshError
-                );
-                return throwError(() => refreshError);
+              catchError((error) => {
+                // console.log('Error al obtener refresh tokens: ', error);
+                return throwError(() => new Error(error));
               })
             );
           }
-          console.error('No hay refresh token: ', error);
-          return throwError(() => error);
+          // console.log('No hay refresh token: ', error);
+          return throwError(() => new Error(error));
         }
+        // console.log('No es estado 401: ', error.errors);
         const errorMessage = error.error?.errors?.length
           ? `${error.error.message}: ${error.error.errors[0]}`
           : `${error.statusText}`;
@@ -127,8 +110,8 @@ export class AuthInterceptor implements HttpInterceptor {
           error.message && errorMessage
             ? `${error.message.trim()} (${errorMessage})`
             : error;
-        console.error(formattedError);
-        return throwError(() => error);
+        // console.error(formattedError);
+        return throwError(() => new Error(error));
       })
     );
   }
